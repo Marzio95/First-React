12052866c4b64a1d28767f47e438be01d84389d4
Marzio95 <marziodellarocca14@gmail.com>
2022-08-01 17:59:40 +0200

    Primo progetto REACT

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// IMPORT
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
+import Button from './shared/design/button/Button';
+import {test as t} from './shared/design/button/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+// VARIABILI che successivamente verranno passate allo state tipo vue
+const name: string | number = 'John Doe'; // string | number è un OR per i tipi di variabile
+let surname: any = 'Della Rocca'; // surname = 10; potrei cambiare il valore di surname in numero grazie al type any che non prevede un tipo preciso di dato
+// let display = false;
+
+// FUNZIONI che successivamente verranno passate nella class App tipo vue
+// const clickAction = () => {display = true; console.log('display: ' + display);}
+
+
+// FUNZIONE APP 
+class App extends Component {
+
+  state = {
+    display: false,
+    name: 'John Doe',
+    surname: 'Della Rocca', 
+    users: []
+  }
+
+  // CHIAMATA AD UN API CON UN METODO ASINCRONO (FETCH)
+  componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+          .then(res => this.setState({users: res}))
+  }
+
+  clickAction = () => {this.setState({display: !this.state.display}); console.log('display: ' + this.state.display)}
+
+  render() {
+    return (
+      <div className="App">
+          <h1>My name is: {this.state.name}</h1>
+          <h1>My surname is: {this.state.surname}</h1>
+          <h2>{t}</h2>
+
+          <Button text="save" classBt="btn-secondary" clickAction={() => console.log('clicked by you')}>
+              <span>Clicked by you</span>
+          </Button>
+          <Button text="Mostra o Nascondi" clickAction={this.clickAction}></Button>
+          <Button text="push"/>
+
+          <div>{this.state.display ? <div>{8 +8}</div> : 'non visualizzare numeri'}</div> {/* ternario dinamico in html */}
+
+          {this.state.users.map((user: any) => <div>{user.name}</div>)}
+      </div>
+    )
+  };
 }
 
 export default App;
---
+++ src/shared/design/button/Button.tsx
@@ -0,0 +1,20 @@
+function Button(props: any) {
+
+    let type: 'button' | 'submit' | 'reset' = props.type || 'button'; // potrei mettere any ma non è molto preciso
+    let classList = ['btn', (props.classBt || 'btn-primary')];
+    let text = props.text || 'Salva';
+
+    const clickHandler = () => {
+        console.log('clicked')
+        props.clickAction && props.clickAction(); // se ci sono clickAction allora eseguo clickAction e se non ci sono clickAction non faccio nulla
+    }
+
+    return <button type={type} 
+                   onClick={clickHandler}
+                   className={classList.join(' ')}>{props.children || text}
+           </button>; // props.children resituisce l html inserito direttamente nel tag Button nell App.tsx
+}
+
+export const test = 'Mario';
+export const m = 'm';
+export default Button;
